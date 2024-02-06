5f4b4ea5d1fde9b85c21a5d9e18afb25ab79fb27
sidd-kishan <aayush.deo1994@gmail.com>
2024-02-07 02:03:59 +0530

    cdc 1 wifi scan and wifi config echo done
    test input:-
    jshfjhafjkhdjfj asdjhasdjhsjka 312213
    
    expected output:-
    
    ----------
    
    jshfjhafjkhdjfj
    
    asdjhasdjhsjka
    
    ----------

--- cherryusb/device/rndis_http_server/src/main.c
+++ cherryusb/device/rndis_http_server/src/main.c
@@ -17,20 +17,9 @@
 
 static volatile absolute_time_t next_wifi_try;
 static volatile absolute_time_t comm_manager;
+char connect_ssid[50], connect_password[80];
+int connect_sec,connect_config;
 
-
-static int scan_result(void *env, const cyw43_ev_scan_result_t *result) {
-	char wlan_scan_buffer[100];
-    if (result) { 
-        int len = sprintf(wlan_scan_buffer, "ssid: %-32s rssi: %4d chan: %3d mac: %02x:%02x:%02x:%02x:%02x:%02x sec: %u\n",
-            result->ssid, result->rssi, result->channel,
-            result->bssid[0], result->bssid[1], result->bssid[2], result->bssid[3], result->bssid[4], result->bssid[5],
-            result->auth_mode);
-		memcpy(read_queue[0].buffer,wlan_scan_buffer,100);
-		read_queue[0].tail=100;
-    }
-    return 0;
-}
 void printline(int cdc,char string[],int len){
 	char buf[2048];
 	//memcpy(buf,"\r\n",2);
@@ -39,34 +28,46 @@ void printline(int cdc,char string[],int len){
 	sprintf(buf,"\r\n%s\r\n\0",string);
 	cdc_acm_data_send_with_dtr(cdc,(uint8_t *)buf,strlen(buf));
 }
+
+static int scan_result(void *env, const cyw43_ev_scan_result_t *result) {
+	char wlan_scan_buffer[120];
+    if (result) { 
+        int len = sprintf(wlan_scan_buffer, "ssid: %-32s rssi: %4d chan: %3d mac: %02x:%02x:%02x:%02x:%02x:%02x sec: %u test-ssid-sec: %d",
+            result->ssid, result->rssi, result->channel,
+            result->bssid[0], result->bssid[1], result->bssid[2], result->bssid[3], result->bssid[4], result->bssid[5],
+            result->auth_mode, CYW43_AUTH_WPA2_AES_PSK);
+		//memcpy(read_queue[0].buffer,wlan_scan_buffer,100);
+		//read_queue[0].tail=len;
+		printline(2,wlan_scan_buffer,len);
+    }
+    return 0;
+}
+
 void core1(){
     //user_init_lwip();
     lwip_init();
-	//while (!netif_is_up(&netif_data)) {
-    //    ;
-    //}
-    //httpd_init();
-	//USB_NOCACHE_RAM_SECTION USB_MEM_ALIGNX uint8_t write_buffer[] = { 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30 };
-	absolute_time_t scan_time = nil_time;
-    bool scan_in_progress = false;
+	
     while(true) {
-        /*if (absolute_time_diff_us(get_absolute_time(), scan_time) < 0) {
-            if (!scan_in_progress) {
-                cyw43_wifi_scan_options_t scan_options = {0};
-                int err = cyw43_wifi_scan(&cyw43_state, &scan_options, NULL, scan_result);
-                if (err == 0) {
-                    //printf("\nPerforming wifi scan\n");
-                    scan_in_progress = true;
-                } else {
-                    //printf("Failed to start scan: %d\n", err);
-                    scan_time = make_timeout_time_ms(5000); // wait 5s and scan again
-                }
-            } else if (!cyw43_wifi_scan_active(&cyw43_state)) {
-                scan_time = make_timeout_time_ms(5000); // wait 5s and scan again
-                scan_in_progress = false; 
-            }
-        }*/
-		printline(2,(char *)read_queue[0].buffer,read_queue[0].tail);
+        /**/
+		//printline(2,(char *)read_queue[0].buffer,read_queue[0].tail);
+		//read_queue[0].tail=0;
+		//memcpy(connect_buf,(char *)read_queue[0].buffer,read_queue[0].tail);
+		//read_queue[0].tail=0;
+		//sscanf(connect_buf, "ssid: %*s password: %*s sec: %d", connect_ssid, connect_password, &connect_sec);
+		connect_config=0;
+		for(int i=0,j=0;i<read_queue[0].tail;i++){
+			if(read_queue[0].buffer[i]==' '){
+				connect_config+=1;
+				j=0;
+			}
+			if(read_queue[0].buffer[i] != ' ' && connect_config==0){
+				connect_ssid[j++]=read_queue[0].buffer[i];
+			} else if(read_queue[0].buffer[i] != ' ' && connect_config==1){
+				connect_password[j++]=read_queue[0].buffer[i];
+			} else {
+				connect_sec=read_queue[0].buffer[i];
+			}
+		}
 		read_queue[0].tail=0;
 		printline(3,(char *)read_queue[1].buffer,read_queue[1].tail);
 		read_queue[1].tail=0;
@@ -92,10 +93,35 @@ int main(void)
 	
 	multicore_launch_core1(core1);
 	
+	absolute_time_t scan_time = nil_time;
+    bool scan_in_progress = false;
+	next_wifi_try = nil_time;
 	while (1) {
 		if (!link_up) {
+			if (absolute_time_diff_us(get_absolute_time(), scan_time) < 0) {
+				if (!scan_in_progress) {
+					cyw43_wifi_scan_options_t scan_options = {0};
+					int err = cyw43_wifi_scan(&cyw43_state, &scan_options, NULL, scan_result);
+					if (err == 0) {
+						//printf("\nPerforming wifi scan\n");
+						scan_in_progress = true;
+					} else {
+						//printf("Failed to start scan: %d\n", err);
+						scan_time = make_timeout_time_ms(2000); // wait 2s and scan again
+					}
+				} else if (!cyw43_wifi_scan_active(&cyw43_state)) {
+					scan_time = make_timeout_time_ms(2000); // wait 2s and scan again
+					scan_in_progress = false; 
+				}
+			}
             if (absolute_time_diff_us(get_absolute_time(), next_wifi_try) < 0) {
-                cyw43_arch_wifi_connect_async("ssid", "password", CYW43_AUTH_WPA2_AES_PSK);
+                //cyw43_arch_wifi_connect_async("ssid", "password", CYW43_AUTH_WPA2_AES_PSK);
+				//connect_buf_len = sprintf(connect_buf, "ssid: %*s password: %*s sec: %d", connect_ssid, connect_password, connect_sec);
+				//connect_buf[connect_buf_len+1]=' ';
+				printline(2,"----------",10);
+				printline(2,connect_ssid,strlen(connect_ssid));
+				printline(2,connect_password,strlen(connect_password));
+				printline(2,"----------",10);
                 next_wifi_try = make_timeout_time_ms(10000);
             }
         } else {
